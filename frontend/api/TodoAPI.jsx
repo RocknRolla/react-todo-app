6e2e43ea745acc6316dc5e4777bb64d3ce933bcc
thebasix <asamuga@gmail.com>
2016-04-29 16:34:16 +0200

    Add Filter functionality

--- frontend/api/TodoAPI.jsx
+++ frontend/api/TodoAPI.jsx
@@ -18,4 +18,29 @@ let setTodos = (todos) => {
     }
 };
 
-module.exports = {getTodos, setTodos};
+let filterTodos = (todos, showCompleted, searchText) => {
+    let filteredTodos = todos;
+
+    filteredTodos = filteredTodos.filter((todo) => {
+        return !todo.completed || showCompleted;
+    });
+
+    filteredTodos = filteredTodos.filter((todo) => {
+        let todoText = todo.text.toLowerCase();
+        return searchText.length === 0 || todoText.indexOf(searchText) > -1;
+    });
+
+    filteredTodos.sort((a, b) => {
+        if (!a.completed && b.completed) {
+            return -1;
+        } else if (a.completed && !b.completed) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+
+    return filteredTodos;
+};
+
+module.exports = {getTodos, setTodos, filterTodos};
